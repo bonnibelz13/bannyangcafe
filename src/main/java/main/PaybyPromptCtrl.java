257ec9e03dbc7c702fc89b26d942c470f55b3890
bonnibelz13 <65070145@kmitl.ac.th>
2023-10-25 18:43:01 +0700

    Merge origin/master
    Conflicts:
    	src/main/java/main/LoginModel.java

--- src/main/java/main/PaybyPromptCtrl.java
+++ src/main/java/main/PaybyPromptCtrl.java
@@ -3,8 +3,16 @@ package main;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.table.DefaultTableModel;
+import model.Account;
+import model.db;
 
 /**
  *
@@ -14,11 +22,14 @@ public class PaybyPromptCtrl implements ActionListener{
     PaybyPromptUI view;
     PayMainCtrl mainCtrl;
     CheckoutCtrl checkoutCtrl;
+    DefaultTableModel orderTableModel;
+    int user_id, order_id;
     
     public PaybyPromptCtrl(PayMainCtrl paymentCtrl, CheckoutCtrl checkoutCtrl){
         
         this.mainCtrl = paymentCtrl;
         this.checkoutCtrl = checkoutCtrl;
+        this.orderTableModel = checkoutCtrl.getOrderTableModel();
         
         initComponents();
     }
@@ -33,6 +44,106 @@ public class PaybyPromptCtrl implements ActionListener{
     
     public JPanel getPanel() {
         return view.getPanel();
+    }
+    
+    
+    public int newOrder(int paymentID) {
+        String sql = "INSERT INTO order_bill (payment_id) VALUES (?)";
+        Connection con = db.getConnection();
+        PreparedStatement ps = null;
+        int generatedOrderID = -1; // กำหนดค่าเริ่มต้น
+
+        try {
+            ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, paymentID);
+
+            int rowsAffected = ps.executeUpdate();
+
+            if (rowsAffected == 1) {
+                ResultSet generatedKeys = ps.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    generatedOrderID = generatedKeys.getInt(1);
+                    System.out.println("Inserted order with order_id: " + generatedOrderID);
+                }
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            System.out.println("Insert Failed.");
+        }
+
+        return generatedOrderID; // คืนค่า order_id ที่ได้รับ
+    }
+    
+    public void findUserID(Account user){
+        System.out.println(user.getUsername());
+        //String
+        Connection con = db.getConnection();
+        String sql= ("SELECT id FROM user WHERE username = '" + user.getUsername()+"'" );
+
+        try (Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);)
+        {while(rs.next()){
+            user_id = rs.getInt("id");
+            System.out.println(user_id);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    
+
+    }
+    
+    public void newOrderItem(int paymentID) {
+        DefaultTableModel dt = (DefaultTableModel) orderTableModel;
+        System.out.println(orderTableModel.getRowCount());
+
+        // เรียกฟังก์ชัน newOrder และรับค่า order_id ที่สร้างขึ้น
+        int order_id = newOrder(paymentID);
+
+        for (int i = 0; i < dt.getRowCount(); i++) {
+            String item = dt.getValueAt(i, 0).toString(); // item Name
+            String qty = dt.getValueAt(i, 3).toString(); // item Qty
+            String totalPrice = dt.getValueAt(i, 4).toString(); // item Total Price
+
+            // select menu_id from menu
+            String sql = "SELECT menu_id FROM menu WHERE menu_name = ?";
+            Connection con = db.getConnection();
+            PreparedStatement ps = null;
+
+            try {
+                ps = con.prepareStatement(sql);
+                ps.setString(1, item); // ตั้งค่าค่า parameter ในคำสั่ง SQL
+
+                ResultSet menuResult = ps.executeQuery();
+
+                if (menuResult.next()) {
+                    int menu_id = menuResult.getInt("menu_id");
+                    System.out.println("MenuID = " + menu_id);
+
+                    String sql_insert = "INSERT INTO order_item (order_id, menu_id, item_qt, item_lineprice) VALUES (?,?,?,?)";
+
+                    try {
+                        ps = con.prepareStatement(sql_insert);
+                        ps.setInt(1, order_id);
+                        ps.setInt(2, menu_id);
+                        ps.setInt(3, Integer.valueOf(qty));
+                        ps.setFloat(4, Float.valueOf(totalPrice));
+
+                        int rowsAffected = ps.executeUpdate();
+
+                        System.out.println("Insert Order_Item to DB completed");
+                        JOptionPane.showMessageDialog(null, "Successfully!");
+
+                    } catch (SQLException ex) {
+                        ex.printStackTrace();
+                        System.out.println("Insert order_item Failed.");
+                    }
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+                System.out.println("Error fetching menu_id.");
+            }
+        }
     }
     
     public void actionPerformed(ActionEvent ev) {
@@ -57,9 +168,16 @@ public class PaybyPromptCtrl implements ActionListener{
                 
                 try {
                      receipt.getTxtPane().print();
+                     // Save to Database.
+                    int paymentID = 2;
+                    newOrder(paymentID);
+                    newOrderItem(paymentID);
+                    
                 } catch (Exception e){
                     
                 }
+                
+                
                 
                 
             }
--- src/main/java/main/PaymentChooseCtrl.java
+++ src/main/java/main/PaymentChooseCtrl.java
@@ -3,6 +3,11 @@ package main;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -10,6 +15,8 @@ import java.util.Date;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.table.DefaultTableModel;
+import model.Account;
+import model.db;
 
 /**
  *
@@ -21,7 +28,8 @@ public class PaymentChooseCtrl implements ActionListener{
     DefaultTableModel orderTableModel;
     ReceiptUI receipt;
     double total, cash, change;
-    String paymentID;
+    String transID;
+    int user_id, order_id;
     
     
     //Default Constructure and Methods
@@ -32,7 +40,7 @@ public class PaymentChooseCtrl implements ActionListener{
         this.cash = checkoutCtrl.getCash();
         this.change = checkoutCtrl.getChange();
         this.orderTableModel = checkoutCtrl.getOrderTableModel();
-        this.paymentID = checkoutCtrl.getPaymentID();
+        this.transID = checkoutCtrl.getPaymentID();
         
         ReceiptUI receipt = new ReceiptUI();
         receipt.getOrderTable().setModel(orderTableModel);
@@ -67,7 +75,7 @@ public class PaymentChooseCtrl implements ActionListener{
         receipt.getTxtPane().setText(receipt.getTxtPane().getText()+"               ");
         receipt.getTxtPane().setText(receipt.getTxtPane().getText()+" \t\t   BANNYANGCHA. CAFE\n");
         receipt.getTxtPane().setText(receipt.getTxtPane().getText()+"\t\t         Tel: 021111111\n");
-        receipt.getTxtPane().setText(receipt.getTxtPane().getText()+"    Payment ID: " + paymentID + "\n");
+        receipt.getTxtPane().setText(receipt.getTxtPane().getText()+"    Transaction ID: " + transID + "\n");
         
         
         Date dd = new Date();
@@ -106,6 +114,107 @@ public class PaymentChooseCtrl implements ActionListener{
 
     }
     
+    
+    public int newOrder(int paymentID) {
+        String sql = "INSERT INTO order_bill (payment_id) VALUES (?)";
+        Connection con = db.getConnection();
+        PreparedStatement ps = null;
+        int generatedOrderID = -1; // กำหนดค่าเริ่มต้น
+
+        try {
+            ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, paymentID);
+
+            int rowsAffected = ps.executeUpdate();
+
+            if (rowsAffected == 1) {
+                ResultSet generatedKeys = ps.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    generatedOrderID = generatedKeys.getInt(1);
+                    System.out.println("Inserted order with order_id: " + generatedOrderID);
+                }
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            System.out.println("Insert Failed.");
+        }
+
+        return generatedOrderID; // คืนค่า order_id ที่ได้รับ
+    }
+    
+    public void findUserID(Account user){
+        System.out.println(user.getUsername());
+        //String
+        Connection con = db.getConnection();
+        String sql= ("SELECT id FROM user WHERE username = '" + user.getUsername()+"'" );
+
+        try (Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);)
+        {while(rs.next()){
+            user_id = rs.getInt("id");
+            System.out.println(user_id);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    
+
+    }
+    
+    public void newOrderItem(int paymentID) {
+        DefaultTableModel dt = (DefaultTableModel) orderTableModel;
+        System.out.println(orderTableModel.getRowCount());
+
+        // เรียกฟังก์ชัน newOrder และรับค่า order_id ที่สร้างขึ้น
+        int order_id = newOrder(paymentID);
+
+        for (int i = 0; i < dt.getRowCount(); i++) {
+            String item = dt.getValueAt(i, 0).toString(); // item Name
+            String qty = dt.getValueAt(i, 3).toString(); // item Qty
+            String totalPrice = dt.getValueAt(i, 4).toString(); // item Total Price
+
+            // select menu_id from menu
+            String sql = "SELECT menu_id FROM menu WHERE menu_name = ?";
+            Connection con = db.getConnection();
+            PreparedStatement ps = null;
+
+            try {
+                ps = con.prepareStatement(sql);
+                ps.setString(1, item); // ตั้งค่าค่า parameter ในคำสั่ง SQL
+
+                ResultSet menuResult = ps.executeQuery();
+
+                if (menuResult.next()) {
+                    int menu_id = menuResult.getInt("menu_id");
+                    System.out.println("MenuID = " + menu_id);
+
+                    String sql_insert = "INSERT INTO order_item (order_id, menu_id, item_qt, item_lineprice) VALUES (?,?,?,?)";
+
+                    try {
+                        ps = con.prepareStatement(sql_insert);
+                        ps.setInt(1, order_id);
+                        ps.setInt(2, menu_id);
+                        ps.setInt(3, Integer.valueOf(qty));
+                        ps.setFloat(4, Float.valueOf(totalPrice));
+
+                        int rowsAffected = ps.executeUpdate();
+
+                        System.out.println("Insert Order_Item to DB completed");
+                        JOptionPane.showMessageDialog(null, "Successfully!");
+
+                    } catch (SQLException ex) {
+                        ex.printStackTrace();
+                        System.out.println("Insert order_item Failed.");
+                    }
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+                System.out.println("Error fetching menu_id.");
+            }
+        }
+    }
+
+    
     public void actionPerformed(ActionEvent ae) {
         
         // cash button pressed.
@@ -114,11 +223,20 @@ public class PaymentChooseCtrl implements ActionListener{
             if (option == JOptionPane.YES_OPTION) {
                 System.out.println("Printing...");
                 drawBill();
+                
                 try {
                      receipt.getTxtPane().print();
+                     
+                    // Save to Database.
+                    
+                    int paymentID = 1;
+                    newOrder(paymentID);
+                    newOrderItem(paymentID);
+                    
                 } catch (Exception e){
                     
                 }
+                
                
                 
             }
